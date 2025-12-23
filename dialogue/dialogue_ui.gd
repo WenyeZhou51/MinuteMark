e0a81ad7be444a0a62c6b7da0a267b8c408f9587
Jialiang Yang <jialiangyang@Jialiangs-MacBook-Pro.local>
2025-12-23 14:45:14 -0600

    Add simple dialogue system with interruption

---
+++ dialogue/dialogue_ui.gd
@@ -0,0 +1,89 @@
+extends Control
+
+@onready var label: Label = $DialoguePanel/DialogueLabel
+@onready var interrupt_indicator: Label = $DialoguePanel/InterruptIndicator
+
+var indicator_faded_in := false
+var was_interruptable := false
+
+
+func _ready():
+	DialogueManager.load_dialogue("res://dialogue/dialogue.json")
+	DialogueManager.line_changed.connect(_on_line_changed)
+	DialogueManager.start("intro")
+
+	interrupt_indicator.visible = false
+
+func _on_line_changed(text: String) -> void:
+	label.text = text
+
+func _process(_delta):
+	if DialogueManager.can_interrupt():
+		_fade_in_indicator()
+	else:
+		_reset_indicator()
+
+
+		
+func _fade_in_indicator():
+	if indicator_faded_in:
+		return
+
+	#print("FADE IN")
+
+	indicator_faded_in = true
+	interrupt_indicator.visible = true
+	interrupt_indicator.text = "..."
+	interrupt_indicator.modulate = Color(1, 1, 1, 0)
+
+	var tween := create_tween()
+	tween.tween_property(
+		interrupt_indicator,
+		"modulate:a",
+		1.0,
+		0.4
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+	tween.finished.connect(_start_indicator_pulse)
+
+func _reset_indicator():
+	if indicator_tween:
+		indicator_tween.kill()
+		indicator_tween = null
+
+	indicator_faded_in = false
+	interrupt_indicator.visible = false
+	interrupt_indicator.modulate.a = 0.0
+
+
+func _input(event):
+	if event.is_action_pressed("dialogue_interrupt"):
+		if DialogueManager.can_interrupt():
+			DialogueManager.start("interrupt")
+			return
+
+	if event.is_action_pressed("ui_accept"):
+		DialogueManager.advance()
+		
+var indicator_tween: Tween
+
+func _start_indicator_pulse():
+	if indicator_tween:
+		indicator_tween.kill()
+
+	indicator_tween = create_tween()
+	indicator_tween.set_loops()
+
+	indicator_tween.tween_property(
+		interrupt_indicator,
+		"modulate:a",
+		0.3,
+		0.6
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	indicator_tween.tween_property(
+		interrupt_indicator,
+		"modulate:a",
+		1.0,
+		0.6
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
